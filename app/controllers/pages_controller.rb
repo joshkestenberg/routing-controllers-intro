789e32c8f8d5d88e1718278e43e27284a62580d5
joshkestenberg <joshkestenberg@gmail.com>
2017-01-30 14:37:43 -0500

    added dynamic route for kittens with method kitten_url to get link for kitten and kittens

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,6 +15,16 @@ class PagesController < ApplicationController
   def kitten
     @header = "Cats!"
 
+    kitten_url
+  end
+
+  def kittens
+    @header = "Tons of Cats!"
+
+    kitten_url
+  end
+
+  def kitten_url
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{(requested_size.to_i / 2)}/cats"
   end
